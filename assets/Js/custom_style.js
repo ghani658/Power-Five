9fe78f789bcf21dee2e042022069be853c74fa1e
Ghani Sheikh <ghani658@gmail.com>
2022-04-04 20:47:26 +0500

    menu

--- assets/Js/custom_style.js
+++ assets/Js/custom_style.js
@@ -52,48 +52,11 @@ $(document).ready(function(){
 			$("a.toggle-btn i").toggleClass('fa-minus');
 		  });
 });
-$('select').each(function(){
-    var $this = $(this), numberOfOptions = $(this).children('option').length;
-  
-    $this.addClass('select-hidden'); 
-    $this.wrap('<div class="select"></div>');
-    $this.after('<div class="select-styled"></div>');
-
-    var $styledSelect = $this.next('div.select-styled');
-    $styledSelect.text($this.children('option').eq(0).text());
-  
-    var $list = $('<ul />', {
-        'class': 'select-options'
-    }).insertAfter($styledSelect);
-  
-    for (var i = 0; i < numberOfOptions; i++) {
-        $('<li />', {
-            text: $this.children('option').eq(i).text(),
-            rel: $this.children('option').eq(i).val()
-        }).appendTo($list);
-    }
-  
-    var $listItems = $list.children('li');
-  
-    $styledSelect.click(function(e) {
-        e.stopPropagation();
-        $('div.select-styled.active').not(this).each(function(){
-            $(this).removeClass('active').next('ul.select-options').hide();
-        });
-        $(this).toggleClass('active').next('ul.select-options').toggle();
-    });
-  
-    $listItems.click(function(e) {
-        e.stopPropagation();
-        $styledSelect.text($(this).text()).removeClass('active');
-        $this.val($(this).attr('rel'));
-        $list.hide();
-        //console.log($this.val());
-    });
-  
-    $(document).click(function() {
-        $styledSelect.removeClass('active');
-        $list.hide();
-    });
-
+$(".mobile-menu a").click(function(){
+    $(".list-menu").slideToggle();
+});
+$('.list-menu > li > i').click(function(){
+    event.preventDefault();
+	$('.submenu').slideUp('slow');
+    $(this).parent().find('.submenu').slideToggle('slow');
 });
